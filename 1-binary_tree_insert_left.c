dd036080c888c166b812148172136b4a0d838f4e
Sotonwa John <johnsotonwa@gmail.com>
2022-09-22 20:39:23 +0100

    Insert left test

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *ptr = NULL;
+    ptr = malloc(sizeof(binary_tree_t));
+    ptr = binary_tree_node(parent, value)
+    /*{
+        node->parent
+        node->left=NULL
+        node->n
+        node->right=NULL
+    }*/;
+    if (parent->left) //if this node already has a left child then:
+    {
+        ptr->left = parent->left; //put the left child of this parent as the new left child of ptr node
+        //parent->left->parent = ptr; //then place the new node (together with its left child) under the parent
+    }
+    parent->left = ptr;
+    return(ptr);
+}
