00945707cc234032221b80555af91208482af24f
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-09-22 23:09:30 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     if (parent->left) //if this node already has a left child then:
     {
         ptr->left = parent->left; //put the left child of this parent as the new left child of ptr node
-        //parent->left->parent = ptr; //then place the new node (together with its left child) under the parent
+        parent->left->parent = ptr; //then place the new node (together with its left child) under the parent
     }
     parent->left = ptr;
     return(ptr);
