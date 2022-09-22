7bfecde60de178a9ea4e08009f51b4a77923787d
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-09-22 23:18:54 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *ptr = NULL;
-    ptr = malloc(sizeof(binary_tree_t));
-    ptr = binary_tree_node(parent, value)
-    /*{
-        node->parent
-        node->left=NULL
-        node->n
-        node->right=NULL
-    }*/;
-    if (parent->left) //if this node already has a left child then:
-    {
-        ptr->left = parent->left; //put the left child of this parent as the new left child of ptr node
-        parent->left->parent = ptr; //then place the new node (together with its left child) under the parent
-    }
-    parent->left = ptr;
-    return(ptr);
+	binary_tree_t *node = NULL;
+
+	if (parent)
+	{
+		node = binary_tree_node(parent, value);
+		if (node)
+		{
+			if (parent->left)
+			{
+				node->left = parent->left;
+				parent->left->parent = node;
+			}
+			parent->left = node;
+		}
+	}
+
+	return (node);
 }
